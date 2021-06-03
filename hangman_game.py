ec520cf3cd5af5f089d074fce063eb321213f601
David Vasquez <vasquezlazoj@gmail.com>
2021-06-02 20:04:55 -0500

    agrege dos listas animales y familia

--- hangman_game.py
+++ hangman_game.py
@@ -1,5 +1,7 @@
 #make hangman game
 import random
+import os
+cls = lambda : os.system("cls")
 body =[''' 
        +---+
 
@@ -106,15 +108,32 @@ body =['''
 
   =========''']
 
-def words():
+def words(a):
     #se encarga de buscar los datos
     # remidn appendm more databases
-    with open("./ahorcado/datos.txt", "r", encoding = "utf-8") as f:
-        list_words = []
-        for line in f:
-            list_words.append(line.strip("\n"))
-        select_word(list_words)
+    if a == 1:
+        print("Lista de palabras ROPA")
+        with open("./base_datos/list_clothes.txt", "r", encoding = "utf-8") as f:
+            list_words = []
+            for line in f:
+                list_words.append(line.strip("\n"))
+            select_word(list_words)
 
+    if a == 2:
+        print("Lista de palabras ROPA")
+        with open("./base_datos/list_animals.txt", "r", encoding = "utf-8") as f:
+            list_words = []
+            for line in f:
+                list_words.append(line.strip("\n"))
+            select_word(list_words)
+
+    if a == 3:
+        print("Lista de palabras ROPA")
+        with open("./base_datos/list_familiar.txt", "r", encoding = "utf-8") as f:
+            list_words = []
+            for line in f:
+                list_words.append(line.strip("\n"))
+            select_word(list_words)
 # draw_man recibe la lista con las palabras
 def select_word(list_words):
     #selecciono la palabra al azar
@@ -122,29 +141,55 @@ def select_word(list_words):
     index_random = random.randint(0,size-1)
     word_random = list_words[index_random]
     tabs ="_" * len(word_random)
-    attemps(word_random,tabs)  
-     
-def options():
-    pass
+    attemps(word_random,tabs)       
+def menu():
+    # para hacer mi menú de las palabras que quiero usar
+    print(""" 
+             (0 0) 
+     ---oOO-- (_) ----oOO---    
+    ╔═════════════════════════╗ 
+    ║ WELCOME TO HANGMAN GAME ║ 
+    ╚═════════════════════════╝ 
+       -------------------
+            |__|__| 
+             || || 
+            ooO Ooo 
 
+    Por: vasquezlazoj
+            """)
+    
+    options = int(input("Seleciona una lista:\n1. Ropa:\n2. Animales\n3. Familiares\n"))
+    if options == 1:
+        cls()
+        words(1)
+    if options == 2:
+        cls()
+        words(2)
+    if options == 3:
+        cls()
+        words(3)
+
+
+    pass
 def draw_body(b):
     print(body[b])
-
-    
 def attemps(word_random, tabs): 
     life = 3
-    print(f'tienes {life} vidas\n')
+    print(f'Empiezas con {life} vidas\n')
     letter_good =""
     body_ini = print(body[0])
     tabs_ini ="_ "*len(word_random)
     b = 0
     print(tabs_ini)
     while life > 0:
-        intento= input("Adivina una letra:\n")
+        intento = input("Adivina una letra: \n")
+        while len(intento) == 0:
+            print("Debes ingresar al menos una letra: ")
+            intento = input()
         intento = intento.lower()
+        cls()
         if life < 3:
             draw_body(b)
-
         letra_repite = 0
         for i in range(len(word_random)):
             if intento in word_random[i]:
@@ -155,34 +200,38 @@ def attemps(word_random, tabs):
         letter_good += intento*letra_repite
         if intento not in word_random:
             b +=2
-            print("Letra incorrecta, tu puedes !")
+            cls()
+            print("Letra incorrecta ! !")
             draw_body(b)
             life -= 1
+            if life == 0:
+                print("***PERDISTE***")
+                print(f'Te quedaste sin vidas, la palabra era {word_random}')        
+                respueta = int(input("Quieres volver a jugar?:\n1. Si\n2: No\n"))
+                cls()
+                if respueta == 1:
+                    menu()
+                else:
+                    break
+                    
+            print(f'te quedan {life} vidas, tu puedes ! ')
         else:
+            cls()
+            print("Muy bien sigue asi")
             draw_body(b)
-        for intento in tabs:
-            
+        for intento in tabs:        
             print(intento, end=" ")
-        
         print()
         if len(letter_good) == len(word_random):
             print("***Lo lograste***")
             print()
-            respuesta = int(input("Quieres volver a jugar?:\n 1: Si\n 2: No\n"))
-            if respuesta == 1:
-                again()
+            respueta = int(input("Quieres volver a jugar?:\n1. Si\n2: No\n"))
+            if respueta == 1:
+                cls()
+                menu()    
             else:
                 break
-        if life == 0:
-            print(f'Te quedaste sin vidas, la palabra era {word_random}')
-            break
 def run():
     pass
-
-def again():
-    words()
-
-
 if __name__=='__main__':
-    words()
-
+    menu()
